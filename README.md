748a33155928c1de0a5d6009527ea09357baa0ca
Guilherme Sonnemann Eckert <guilhermeeckert@Guilhermes-MacBook-Pro.local>
2020-07-27 01:18:34 -0700

    added: axios, request api using axios.

---
+++ README.md
@@ -0,0 +1,100 @@
+# **Git Explorer**
+
+
+<p align="center">
+  <img width="150" height="100" src="img/logo.png">
+</p>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/commonality/readme-inspector.svg">
+  <img alt="Top language" src="https://img.shields.io/github/languages/top/commonality/readme-inspector.svg">
+</p>
+
+---
+
+<h2 align="center">
+	 🚀 Responsive Git Explorer for study purposes. 🚀
+
+   [Demonstration here](https://twitter-clone-guilherme-509609.netlify.app/)
+</h2>
+
+
+---
+
+ <p align="center">
+  <img width="500" height="450" src="img/gitexplorer.png">
+</p>
+
+
+</div>
+
+<h4 align="center">
+	🚧   🚀 Under construction ...  🚧
+</h4>
+
+
+## 🛠 Technology
+
+The following tools were used in the construction of the project:
+
+- [Node.js](https://nodejs.org/en/)
+- [React](https://pt-br.reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+
+## Features
+
+- [ ] Serach Repositories
+
+
+### Prerequisites
+
+Before you begin, you will need to have the following tools installed on your computer:
+
+ - [Git](https://git-scm.com)
+ - [Node.js](https://nodejs.org/en/)
+
+In addition, it is good to have an editor to work with the code like
+
+ - [VSCode](https://code.visualstudio.com/)
+
+
+## Project
+
+
+#### Dependencies
+
+
+- Styled-components
+- React-router-dom
+- React-icons
+- Polished
+- Axius
+
+
+### 🎲 Running the the project
+
+``` bash
+# Clone this repository
+$ git clone https://github.com/guilhermeseckert/Github_Explorer.git
+
+# Access the project folder in the / cmd terminal
+$ cd Github_Explorer
+
+# Install the dependencies
+$ yarn
+
+# Run the application in development mode
+$ yarn start
+
+```
+
+
+
+---
+Made with by Guilherme S.Eckert :wave: [Get in touch!](https://www.linkedin.com/in/guilherme-eckert/)
+
+
+
+
+
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,40 +1,61 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  // eslint-disable-next-line camelcase
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    // eslint-disable-next-line camelcase
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="github Explorer" />
       <Title>Explore repositories on GitHub</Title>
-      <Form>
-        <input placeholder="Git user" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Git repository"
+        />
         <button type="submit">Search</button>
       </Form>
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars0.githubusercontent.com/u/53009976?s=460&u=274139a7a8e844d7308622c898361bb30f1850fb&v=4"
-            alt="Guilherme"
-          />
-          <div>
-            <strong>guilherme</strong>
-            <p>blablabla</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars0.githubusercontent.com/u/53009976?s=460&u=274139a7a8e844d7308622c898361bb30f1850fb&v=4"
-            alt="Guilherme"
-          />
-          <div>
-            <strong>guilherme</strong>
-            <p>blablabla</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
